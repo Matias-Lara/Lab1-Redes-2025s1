50082e29a0755ed82793b66d4a6b099dfb3e0bc2
Matias Lara <matias.laraplaza2004@gmail.com>
2025-06-24 01:11:51 -0400

    asimetrico.cpp y conclusiones de este en el readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Lab1-Redes-2025s1
+## Como usar crypto++:
 1. Descarguen la ultima version de cripto https://www.cryptopp.com/#download
 2. Creen una carpeta en C: llamada dev (o como quieran) y metan el zip descomprimido q descargaron les deberia qquedar algo como C/dev/Criptopp890
 3. Descarguen el zip de este git https://github.com/noloader/cryptopp-pem
@@ -8,3 +9,11 @@
 7. Ejecuten 'make'
 8. Ejecuten 'make install PREFIX=/mingw64'
 9. Y listo, tendran la libreria Cryptopp(crypto++) con el archivo pem.h que sirve para cargar claves pem en c++
+10. Compilar los archivos con 'g++ nombre.cpp -o nombre -mconsole -lcryptopp'
+
+## Conclusiones sobre asimetrico.cpp:
+Al compilar y ejecutar el .exe nos damos cuenta q la firma digital mas el mensaje exceden el tamanio posible que RSA puede cifrar (214bytes) por lo tanto la ejecucion captura el error y lo muestra por consola.
+La solucion a esto es implementar de forma hibrida los dos algoritmos (simetrico y asimetrico) y esta es la respuesta de la pregunta 4 (la del canal seguro)
+
+## que falta de momento?
+Faltaria el desarollo de esta solucion de la q hablo, es decir la 4. y crear el informe en latex
--- asimetrico.cpp
+++ asimetrico.cpp
@@ -1,29 +1,80 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <cryptopp/rsa.h> // RSA
-#include <cryptopp/files.h> // Para FileSource
-#include <cryptopp/osrng.h> // Para el generador de numeros aleatorios
-#include <cryptopp/base64.h> // Para Base64
-#include <cryptopp/pssr.h> // Para el esquema de firma
-#include <cryptopp/sha.h> // Para el hash SHA256
+#include <cryptopp/rsa.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/base64.h>
+#include <cryptopp/files.h>
 #include <cryptopp/pem.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/pssr.h>
 
 using namespace CryptoPP;
 using namespace std;
 
-// Función para cargar una clave pública desde un archivo .pem
+// funciones para cargar las claves .pem
 void LoadPublicKey(const string& filename, RSA::PublicKey& key) {
     FileSource file(filename.c_str(), true);
     PEM_Load(file, key);
 }
-
-// Función para cargar una clave privada desde un archivo .pem
 void LoadPrivateKey(const string& filename, RSA::PrivateKey& key) {
     FileSource file(filename.c_str(), true);
     PEM_Load(file, key);
 }
 
-int main(){
+int main() {
+    try {
+        // nombre de los archivos .pem
+        string gm_publica_file = "gm_publica.pem";
+        string lyra_privada_file = "lyra_privada.pem";
+        string mensaje = "Los archivos antiguos, código MPSH476, revelan la ubicación del séptimo pergamino perdido.";
+
+        cout << "Mensaje original: " << mensaje << endl;
+
+        // cargar las claves de lyra
+        RSA::PublicKey gmPublicKey;
+        LoadPublicKey(gm_publica_file, gmPublicKey);
+
+        RSA::PrivateKey lyraPrivateKey;
+        LoadPrivateKey(lyra_privada_file, lyraPrivateKey);
+
+        // firmar el mensaje q lyra quiere enviar con su clave publica
+        cout << "Firmando el mensaje con la clave privada de Lyra..." << endl;
+        AutoSeededRandomPool rng;
+        string firma;
+        // motor para firmar con el esquema de cifrado OAEP
+        RSASSA_PKCS1v15_SHA256_Signer signer(lyraPrivateKey);
+        // pipeline donde el filtro firma el mensaje 
+        StringSource(mensaje, true,
+            new SignerFilter(rng, signer, 
+                new StringSink(firma)
+            )
+        );
+
+        // preparar el mensaje para cifrar
+        string paquete = mensaje + firma;
+        cout << "Tamanio del paquete (mensaje+firma): " << paquete.length() << " bytes." << endl;
+
+        // cifrar el paquete con la clave publica del gm
+        cout << "Cifrando el paquete con la clave publica del Gran Maestro..." << endl;
+
+        // pipeline que cifra el mensaje con SHA
+        string paquete_cifrado;
+        RSAES_OAEP_SHA_Encryptor encriptador(gmPublicKey);
+        StringSource(paquete, true,
+            new PK_EncryptorFilter(rng, encriptador,
+                new StringSink(paquete_cifrado)
+            )
+        );
+
+        // este cout no aparecera si el paquete es dms grande por que entrara en el catch
+        cout << "Paquete cifrado sin problemas." << endl;
+
+    } catch (const CryptoPP::Exception& e) {
+        // la ejecucion termina aqui cuando el PK_EncryptorFilter falla.
+        cerr << "\nError en Crypto++: " << e.what() << endl;
+        return 1;
+    }
     return 0;
 }
